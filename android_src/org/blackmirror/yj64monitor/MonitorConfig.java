2e9da52eb32f151a80a59bb649caae27500a699a
concreteballs <Arnold_Superstein@proton.me>
2026-09-23 17:11:33 +0200

    android: add YJ-64 APK build pipeline (android_src/org/blackmirror/yj64monitor/MonitorConfig.java)

---
+++ android_src/org/blackmirror/yj64monitor/MonitorConfig.java
@@ -0,0 +1,6 @@
+package org.blackmirror.yj64monitor;
+
+public final class MonitorConfig {
+    private MonitorConfig() {}
+    public static final String TARGET_PACKAGE = "org.blackmirror.blackmirror";
+}
